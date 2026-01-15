415d1be4ee2caa438e5b608eeef444259a15fca5
혜전 <gracefield0814@gmail.com>
2026-01-15 12:40:23 +0900

    image, python file add

---
+++ python/test.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+import pymysql
+
+fastAPIAddress = "172.16.250.148"
+
+app = FastAPI()
+
+def get_conn():
+    return pymysql.connect(
+        host="127.0.0.1",
+        port=3307,
+        user="zero",
+        password="Qwer1234!@#$",
+        database="seatup",
+        charset="utf8mb4",
+        cursorclass=pymysql.cursors.DictCursor,
+        autocommit=True,
+    )
+
+@app.get("/ping-db")
+def ping_db():
+    conn = get_conn()
+    try:
+        with conn.cursor() as cur:
+            cur.execute("SELECT 1 AS ok;")
+            return cur.fetchone()
+    finally:
+        conn.close()
+@app.get("/insert/{text}")
+async def insert(text:str):
+    conn = get_conn()
+    curs = conn.cursor()
+
+    try:
+        sql = "insert into grade (grade_name, grade_create_date) values (%s, now())" 
+        curs.execute(sql, (text,))
+        conn.commit()
+        return {'results' : 'Success'}
+    except Exception as e: 
+        return {'results' : f'Error :{e}'}
+    finally:
+        conn.close()
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host=fastAPIAddress, port=8000)
+
