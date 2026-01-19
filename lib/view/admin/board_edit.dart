d77499a5ac25e74e193e7a3ae428a0698bf31937
gracefield <gracefield0814@gmail.com>
2026-01-19 11:47:01 +0900

    어드민 탭 링크 test

--- lib/view/admin/board_edit.dart
+++ lib/view/admin/board_edit.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:seatup_app/util/global_data.dart';
 
-class BoardEdit extends StatelessWidget {
+class BoardEdit extends StatefulWidget {
   const BoardEdit({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  State<BoardEdit> createState() => _BoardEditState();
 }
+
+class _BoardEditState extends State<BoardEdit> {
+  // === Property ===
+  String imageUrl = "${GlobalData.url}/images/viewOne";
+  String stockSelectAllUrl = "${GlobalData.url}/stock/selectAll";
+  List<int> _imageList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  } // build
+} // class
