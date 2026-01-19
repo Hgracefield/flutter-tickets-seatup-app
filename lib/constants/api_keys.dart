4ee9aa08b032b6a6b2fbbab5af5f1561771dd467
오성의 <34715603@naver.com>
2026-01-20 03:38:16 +0900

    api key 보안 위해 분리해서 작성

---
+++ lib/constants/api_keys.dart
@@ -0,0 +1,4 @@
+// 실제 인증키는 커밋하지 않을 것
+// 팀원 개인 로컬 환경에서만 설정하기
+
+const String weatherServiceKey = '<SERVICE_KEY>';
--- lib/view/user/main_page_home.dart
+++ lib/view/user/main_page_home.dart
@@ -4,6 +4,7 @@ import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 // import 'package:seatup_app/view/user/category.dart';
 import 'package:http/http.dart' as http;
+import 'package:seatup_app/constants/api_keys.dart';
 
 class MainPageHome extends StatefulWidget {
   const MainPageHome({super.key});
@@ -25,13 +26,11 @@ class _MainPageHomeState extends State<MainPageHome> {
 
   // Functions ---
   Future<void> fetchWeather() async { // 기상청 API
-    const serviceKey =
-        '보안된 키로 재삽입 예정';
     const url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0';
 
     final uri = Uri.parse(
       '$url/getVilageFcst' // URL -> 단기예보조회
-      '?serviceKey=$serviceKey' // 인증키
+      '?serviceKey=$weatherServiceKey' // 인증키
       '&pageNo=1' // 페이지 번호
       '&numOfRows=10' // 한 페이지 결과 수
       '&dataType=JSON' // 요청자료형식(XML/JSON)
