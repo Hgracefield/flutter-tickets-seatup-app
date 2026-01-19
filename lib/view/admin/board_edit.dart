173bfa2ed8e1e481c0d77e4200885e3426ccb208
gracefield <gracefield0814@gmail.com>
2026-01-19 12:03:23 +0900

    admin 링크 삭제

--- lib/view/admin/board_edit.dart
+++ lib/view/admin/board_edit.dart
@@ -1,21 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:seatup_app/util/global_data.dart';
 
-class BoardEdit extends StatefulWidget {
+class BoardEdit extends StatelessWidget {
   const BoardEdit({super.key});
 
   @override
-  State<BoardEdit> createState() => _BoardEditState();
-}
-
-class _BoardEditState extends State<BoardEdit> {
-  // === Property ===
-  String imageUrl = "${GlobalData.url}/images/viewOne";
-  String stockSelectAllUrl = "${GlobalData.url}/stock/selectAll";
-  List<int> _imageList = [];
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold();
-  } // build
-} // class
+    return const Placeholder();
+  }
+}
