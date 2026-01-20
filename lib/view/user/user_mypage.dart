daf4252a23c05eaa1ba29038008e4fa3ef2de71c
SuJin Park <overblac@naver.com>
2026-01-20 18:07:29 +0900

    user 마이페이지 경로연결 로그아웃까지

--- lib/view/user/user_mypage.dart
+++ lib/view/user/user_mypage.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seatup_app/view/user/purchase_history.dart';
+import 'package:seatup_app/view/user/review_list.dart';
+import 'package:seatup_app/view/user/sell_history.dart';
+import 'package:seatup_app/view/user/sell_register.dart';
+import 'package:seatup_app/view/user/seller_to_admin_chat.dart';
+import 'package:seatup_app/view/user/transaction_review_list.dart';
+import 'package:seatup_app/view/user/user_chat_list.dart';
+import 'package:seatup_app/view/user/user_login.dart';
+import 'package:seatup_app/view/user/user_to_user_chat.dart';
 import 'package:seatup_app/view/widgets/logout_button.dart';
 import 'package:seatup_app/view/widgets/my_greeting_banner.dart';
 import 'package:seatup_app/view/widgets/my_page_menu.dart';
-
-
+import 'package:seatup_app/vm/storage_provider.dart';
 
 class UserMypage extends ConsumerStatefulWidget {
   const UserMypage({super.key});
@@ -18,6 +26,18 @@ class _UserMypageState extends ConsumerState<UserMypage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        child: const Icon(Icons.chat, color: Colors.white),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => const SellerToAdminChat(),
+            ),
+          );
+        },
+      ),
       appBar: AppBar(
         title: const Text("MY티켓"),
         centerTitle: true,
@@ -25,9 +45,7 @@ class _UserMypageState extends ConsumerState<UserMypage> {
         elevation: 0,
         foregroundColor: Colors.black,
       ),
-      body:
-       ListView(
-        
+      body: ListView(
         children: [
           const MyGreetingBanner(),
           const SizedBox(height: 10),
@@ -38,74 +56,70 @@ class _UserMypageState extends ConsumerState<UserMypage> {
             child: Column(
               children: [
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12,horizontal: 10),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 12,
+                    horizontal: 10,
+                  ),
                   child: MyPageMenuSection(
                     title: "내가 구매한 상품",
                     items: [
                       MyPageMenuItemData(
                         text: "구매 이력 관리",
-                        onTap: () {
-                          // TODO: 구매 이력 관리 화면 이동
-                        },
+                        onTap:() => _push(PurchaseHistory()),
                       ),
                     ],
                   ),
                 ),
-                const Divider(
-                  thickness: 1,
-                  color: Color(0xFFE6E6E6),
-                ),
+                const Divider(thickness: 1, color: Color(0xFFE6E6E6)),
                 
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12,horizontal: 10),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 12,
+                    horizontal: 10,
+                  ),
                   child: MyPageMenuSection(
                     title: "내가 판매한 상품",
                     items: [
-                      MyPageMenuItemData(
-                        text: "판매관리",
-                        onTap: () {},
-                      ),
+                      MyPageMenuItemData(text: "판매 등록", onTap: () => _push(SellRegister())),
+                      MyPageMenuItemData(text: "판매 내역", onTap: () => _push(SellHistory())),
                     ],
                   ),
                 ),
-                const Divider(
-                  thickness: 1,
-                  color: Color(0xFFE6E6E6),
-                ),
+                const Divider(thickness: 1, color: Color(0xFFE6E6E6)),
                 
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12,horizontal: 10),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 12,
+                    horizontal: 10,
+                  ),
                   child: MyPageMenuSection(
                     title: "",
                     items: [
                       MyPageMenuItemData(
-                        text: "1:1 문의하기",
-                        onTap: () {},
+                        text: "거래 채팅 리스트",
+                        onTap: () => _push(UserChatList())
                       ),
-                      MyPageMenuItemData(
-                        text: "1:1 문의 내역",
-                        onTap: () {},
-                      ),
-                      MyPageMenuItemData(
-                        text: "나의 후기",
-                        onTap: () {},
-                      ),
+
+                      MyPageMenuItemData(text: "관람 후기", onTap: () => 
+                      _push(ReviewList())),
+                      MyPageMenuItemData(text: "거래 후기", onTap: () =>
+                      _push(TransactionReviewList())),
                     ],
                   ),
                 ),
-                const Divider(
-                  thickness: 1,
-                  color: Color(0xFFE6E6E6),
-                ),
+                const Divider(thickness: 1, color: Color(0xFFE6E6E6)),
                 
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12,horizontal: 10),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 12,
+                    horizontal: 10,
+                  ),
                   child: MyPageMenuSection(
                     title: "회원정보",
                     items: [
                       MyPageMenuItemData(
                         text: "회원정보수정",
-                        onTap: () {},
+                        onTap: () {},// 페이지 없어서 보기좋게 넣어놓기만함
                       ),
                     ],
                   ),
@@ -120,7 +134,17 @@ class _UserMypageState extends ConsumerState<UserMypage> {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: LogoutButton(
               onTap: () {
-                // TODO: 로그아웃 처리
+                final box = ref.read(storageProvider);
+
+                box.remove('userIsLogin');
+                box.remove('user_id');
+                box.remove('user_name');
+                box.remove('user_email');
+
+                // 로그인 페이지로 이동 뒤로가기 방지
+                Navigator.pushAndRemoveUntil(
+                  context, MaterialPageRoute(builder: (_) => const UserLogin()),
+                (route) => false,);
               },
             ),
           ),
@@ -130,7 +154,10 @@ class _UserMypageState extends ConsumerState<UserMypage> {
             width: double.infinity,
             color: Colors.white,
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 20),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 20,
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -149,13 +176,16 @@ class _UserMypageState extends ConsumerState<UserMypage> {
                       fontWeight: FontWeight.w500,
                       height: 1.3,
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
+  }
+  void _push(Widget page) {
+    Navigator.push(context, MaterialPageRoute(builder: (_) => page));
   }
 }
